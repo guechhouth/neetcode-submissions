29f75944580391af594f09b175fc3b37ff063d81
Chhiv Guech Houth <131672161+guechhouth@users.noreply.github.com>
2026-08-20 09:17:33 -0500

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group_map = defaultdict(list) # key=tuple of dict, val=list of words anagram
+        
+        for word in strs:
+            """
+            w = defaultdict(int) # w can be in different orders, need to sort   
+            sorted_word = sorted(word)
+            for c in sorted_word:
+                w[c] += 1
+            # add to group_map
+            group_map[tuple(w.items())].append(word)
+            """
+        
+            sorted_word = sorted(word) # this return back lists
+            # add to group_map
+            group_map[tuple(sorted_word)].append(word)
+          
+        return list(group_map.values()) # .values give object
+
+                
+
+        
